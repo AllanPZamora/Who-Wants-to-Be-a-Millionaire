74e09c987455a6fcffbbe6eda70cbd08b5334c96
AllanPZamora <gelgelacc1@gmail.com>
2023-11-11 20:38:56 +0800

    added another form

--- Finals/Finals/Game.Designer.vb
+++ Finals/Finals/Game.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class Form1
+Partial Class Game
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
@@ -28,11 +28,10 @@ Partial Class Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(627, 381)
+        Me.ClientSize = New System.Drawing.Size(513, 450)
         Me.Name = "Form1"
         Me.Text = "Form1"
         Me.ResumeLayout(False)
 
     End Sub
-
 End Class
